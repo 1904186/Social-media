12b37c6c491721fb1e204f0861c71c7ed982a655
Sahil Anjum <67681012+sahil-1904219@users.noreply.github.com>
2021-08-24 00:31:10 +0530

    Add files via upload

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,85 @@
+import "./post.css";
+import { MoreVert } from "@material-ui/icons";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";//it is a tool to make requests
+import { format } from "timeago.js";//it is uded to format document in ago format
+import { Link } from "react-router-dom";//it is used to go from one page to another without refreshing it
+import { AuthContext } from "../../context/AuthContext";
+
+export default function Post({ post }) {
+  const [like, setLike] = useState(post.likes.length);//here we r checking how many likes it have
+  const [isLiked, setIsLiked] = useState(false);//here we r checking if i have liked or not
+  const [user, setUser] = useState({});//storing the user
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;// importing thr photos
+  const { user: currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    setIsLiked(post.likes.includes(currentUser._id));
+  }, [currentUser._id, post.likes]);//here we r checking if the post is liked or not
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?userId=${post.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [post.userId]);
+
+  const likeHandler = () => {
+    try {
+      axios.put("/posts/" + post._id + "/like", { userId: currentUser._id });
+    } catch (err) {}
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };// here we have written liken function to increase or decrease like according to condition
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <Link to={`/profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                src={
+                  user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "person/noAvatar.png"
+                }
+                alt=""
+              />
+            </Link>
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={PF + post.img} alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img
+              className="likeIcon"
+              src={`${PF}like.png`}
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src={`${PF}heart.png`}
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCounter">{like} people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
